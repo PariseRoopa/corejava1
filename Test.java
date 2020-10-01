5dd1de29874cbef660d1bdf8b4353f8762ad3283
PariseRoopa <72015538+PariseRoopa@users.noreply.github.com>
2020-10-01 12:56:58 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,17 @@
+package corejavaprograms;
+
+public class Test 
+	{ 
+	public static void main(String args[]) 
+	{ 
+	new Thread(new Runnable() 
+	{ 
+	@Override
+	public void run() 
+	{ 
+	System.out.println("New thread created"); 
+	} 
+	}).start(); 
+	} 
+	}
+
---
+++ sum.java
@@ -0,0 +1,16 @@
+package corejavaprograms;
+
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{          
+		      int a = 65;  
+		        int b = 35;  
+		       
+		        System.out.println("The sum of a and b is = " + Integer.sum(a, b));  
+		    }  
+		}  
+
+	}
+
